a75b3a70c96c7c11602a4ff140ce5ce8ec4bf5a8
David Tarrant <davetaz@theodi.org>
2024-04-16 11:20:32 +0100

    Updating loaders

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,16 @@
 import { RAGApplication } from './core/rag-application.js';
 import { RAGApplicationBuilder } from './core/rag-application-builder.js';
 import { TextLoader } from './loaders/text-loader.js';
-import { YoutubeLoader } from './loaders/youtube-loader.js';
 import { PdfLoader } from './loaders/pdf-loader.js';
 import { WebLoader } from './loaders/web-loader.js';
 import { JsonLoader } from './loaders/json-loader.js';
 import { BaseLoader } from './interfaces/base-loader.js';
-import { YoutubeChannelLoader } from './loaders/youtube-channel-loader.js';
-import { YoutubeSearchLoader } from './loaders/youtube-search-loader.js';
 import { SitemapLoader } from './loaders/sitemap-loader.js';
 import { BaseModel } from './interfaces/base-model.js';
 import { SIMPLE_MODELS } from './global/constants.js';
 import { OpenAi } from './models/openai-model.js';
 import { OpenAiMod } from './models/openai-model-mod.js';
 import { Gemini } from './models/gemini-model.js';
-import { ConfluenceLoader } from './loaders/confluence-loader.js';
 import { AdaEmbeddings } from './embeddings/ada-embeddings.js';
 import { CohereEmbeddings } from './embeddings/cohere-embeddings.js';
 import { OpenAi3LargeEmbeddings } from './embeddings/openai-3large-embeddings.js';
@@ -22,4 +18,12 @@ import { OpenAi3SmallEmbeddings } from './embeddings/openai-3small-embeddings.js
 import { GeminiEmbeddings } from './embeddings/gemini-embeddings.js';
 import { Mistral } from './models/mistral-model.js';
 import { HuggingFace } from './models/huggingface-model.js';
-export { RAGApplication, RAGApplicationBuilder, TextLoader, YoutubeLoader, PdfLoader, WebLoader, JsonLoader, BaseLoader, YoutubeChannelLoader, YoutubeSearchLoader, SitemapLoader, ConfluenceLoader, BaseModel, SIMPLE_MODELS, OpenAi, OpenAiMod, Gemini, AdaEmbeddings, CohereEmbeddings, OpenAi3LargeEmbeddings, OpenAi3SmallEmbeddings, GeminiEmbeddings, Mistral, HuggingFace, };
+/*
+ * If you want them, you need to import them yourself!
+ *
+ * import { YoutubeLoader } from './loaders/youtube-loader.js';
+ * import { YoutubeChannelLoader } from './loaders/youtube-channel-loader.js';
+ * import { YoutubeSearchLoader } from './loaders/youtube-search-loader.js';
+ * import { ConfluenceLoader } from './loaders/confluence-loader.js';
+*/
+export { RAGApplication, RAGApplicationBuilder, TextLoader, PdfLoader, WebLoader, JsonLoader, BaseLoader, SitemapLoader, BaseModel, SIMPLE_MODELS, OpenAi, OpenAiMod, Gemini, AdaEmbeddings, CohereEmbeddings, OpenAi3LargeEmbeddings, OpenAi3SmallEmbeddings, GeminiEmbeddings, Mistral, HuggingFace, };
